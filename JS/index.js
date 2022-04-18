c12f988ca253260ede8d3c288fedc2db00c1ef0f
apsierrame <apsierramendoza@gmail.com>
2022-04-17 20:27:30 -0500

    Estilos completados

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,26 @@
-import contactameObserver from './dark-bg.js';
-
+import {navShow} from './exports.js';
+const header = document.querySelector('.header__top-bar');
+const navmarks = document.querySelectorAll('.navmarks__dot');
 const contactame = document.querySelector('#contactame');
+
+
+//Cambio de color en los navmarks y en el header
+
+const contactameObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            header.classList.add('header__negative');
+            navmarks.forEach(navmark => {
+                navmark.classList.add('navmarks__dot--negative');
+            })
+        } else {
+            header.classList.remove('header__negative');
+            navmarks.forEach(navmark => {
+                navmark.classList.remove('navmarks__dot--negative');
+            })
+        }
+    });
+}, { threshold: 0.5 } );
 contactameObserver.observe(contactame);
+navShow();
+
